5a75d65a693b4b3d39a866e435aa6db25f90044e
ycj28 <1392190220@qq.com>
2023-02-28 17:42:01 +0800

    面包屑debug

--- src/components/common/Breadcrumb.vue
+++ src/components/common/Breadcrumb.vue
@@ -3,7 +3,7 @@
       <el-card>
          <el-breadcrumb separator-class="el-icon-arrow-right">
             <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
-            <el-breadcrumb-item v-for="(item, index) in $route.matched" :key="index">
+            <el-breadcrumb-item v-for="(item, index) in this.$route.matched" :key="index">
                {{ item.name }}</el-breadcrumb-item>
          </el-breadcrumb>
       </el-card>
